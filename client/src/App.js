a21120721c71efe0d3f3489e9dcc7f700fc993c6
Kim dongchan <dongchan1583@gmail.com>
2021-10-21 09:59:13 +0900

    app css 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
 import { useState } from 'react';
-
 import './App.css';
 import SingIn from './Auth/SignIn';
-
 import HomePage from './Pages/HomePage';
-
 import Footer from './Components/Footer';
 import { Route, Switch } from 'react-router';
 import Register from './Auth/Register';
+import Board from './Pages/Board';
 
 const App = () => {
   return (
     <>
       <HomePage />
-
       <Switch>
         <Route path="/account/login">
           <SingIn />
         </Route>
         <Route path="/account/register">
           <Register />
+        </Route>
+        <Route path="/board">
+          <Board />
         </Route>
         <Footer />
       </Switch>
---
+++ client/src/Pages/Board.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css'
+
+
+
+const Board = () => {
+
+    return (
+        
+          <div className="board-list">
+            게시판 리스트
+        </div>
+       
+    )
+}
+
+export default Board
