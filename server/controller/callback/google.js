1caef43e9a26217a372d016e9cd8d0303a94553c
Hyun H <raidi9069@gmail.com>
2021-10-26 15:52:27 +0900

    google Oauth

--- server/controller/callback/google.js
+++ server/controller/callback/google.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 
 const clientId = process.env.GOOGLE_CLIENT_ID;
 const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+const redirect = "http://gearlog-db.s3-website.ap-northeast-2.amazonaws.com/";
 const axios = require("axios");
 
 module.exports = (req, res) => {
+  const code = req.body.authorizationCode;
   axios({
     method: "POST",
-    url: `https://accounts.google.com/o/oauth2/token?grant_type=authorization_code&client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=http://gear-log.s3-website.ap-northeast-2.amazonaws.com&code=${req.body.authorizationCode}`,
+    url: `https://accounts.google.com/o/oauth2/token?grant_type=authorization_code&client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=${redirect}&code=${code}`,
   })
     .then((response) => {
       // console.log(response);
