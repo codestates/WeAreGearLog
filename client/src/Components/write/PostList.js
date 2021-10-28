685ea7e8695cd77fa6e3fa27a31c66e36f5c7e70
Kim dongchan <dongchan1583@gmail.com>
2021-10-28 10:04:49 +0900

    게시판 리스트 1차 작업

--- client/src/Components/write/PostList.js
+++ client/src/Components/write/PostList.js
@@ -1,10 +1,47 @@
 import React  from 'react';
+import { Button } from '@material-ui/core';
 
-const PostList = () => {
+const PostList = (props) => {
 
     return (
         <div>
-            게시글 페이지 컴포넌트
+           <table style={{ width: '100%'}}>
+                <colgroup>
+                <col width='10%' />
+                <col width='70%' />
+                <col width='10%' />
+                <col width='10%' />
+                </colgroup>
+                <tbody>
+                    <tr>
+                        <th>번호</th>
+                        <th>제목</th>
+                        <th>조회수</th>
+                        <th></th>
+                    </tr>
+                </tbody>
+                <tbody>
+                {props.board && props.board.map((article)=> (
+                     <tr key={article.id}>
+                     <td>{article.id}</td>
+       
+                     <td onClick={() => props.handleArticleTitleClick(article.id)}>
+                       {article.title}
+                       &nbsp;
+                       {props.commentLength[article.id] > 0 &&
+                         `[${props.commentLength[article.id]}]`}
+                     </td>
+       
+                     <td>{article.views}</td>
+                     <td>
+                       <Button onClick={() => props.handleDeleteClick(article.id)}>
+                         X
+                       </Button>
+                     </td>
+                   </tr>
+                ))}
+                </tbody>
+           </table>
         </div>
     )
 
