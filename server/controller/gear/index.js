07944899ea54e137f9eb9bb8d9ba91da4e170bd7
Hyun H <raidi9069@gmail.com>
2021-10-31 21:38:34 +0900

    server : { date:10/31 , 작성한 기능 : user post comment 테이블간 관계 생성 }

--- server/controller/gear/index.js
+++ server/controller/gear/index.js
@@ -1,3 +1,32 @@
+const { gear } = require("../../models");
+
 module.exports = {
-  gear: require("./gear"),
+  item: (req, res) => {
+    const id = req.params.id;
+
+    gear.findOne({ where: { id: id } }).then((data) => {
+      //   console.log(data.dataValues);
+      res.status(200).json({
+        content: data.dataValues,
+      });
+    });
+  },
+  list: async (req, res) => {
+    const list = await gear.create({
+      title: "",
+      intro: "",
+      src: "",
+      title1: "",
+      intext: "",
+      interviewer: "",
+      li1: "",
+      li2: "",
+      li3: "",
+      li4: "",
+      final: "",
+      ftext: "",
+    });
+
+    res.status(200).send("OK");
+  },
 };
