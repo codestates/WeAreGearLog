5b9c42b80908b3dd59743c9a8f9ffa96eb160550
Hyun H <raidi9069@gmail.com>
2021-11-02 17:43:02 +0900

    server : { date:11/2 , 작성한 기능 :게시글 수정 응답 개선2 }

--- server/controller/post/delete.js
+++ server/controller/post/delete.js
@@ -4,6 +4,9 @@ const { isAuthorized } = require("../tokenFunctions");
 module.exports = {
   deleteComment: async (req, res) => {
     const readerData = isAuthorized(req);
+    if (!readerData) {
+      return res.status(401).send("로그인 중이 아니거나 만료된 토큰");
+    }
     const readerId = readerData.id;
     const { postId, commentId } = req.body;
     const commentData = await comment.findOne({ where: { id: commentId } });
@@ -24,6 +27,9 @@ module.exports = {
   deletePost: async (req, res) => {
     // 1.댓글삭제(해당postId가진 댓글들 전부) 2. 좋아요 삭제(해당postId가진 좋아요들 전부) 3. 게시글삭제
     const readerData = isAuthorized(req);
+    if (!readerData) {
+      return res.status(401).send("로그인 중이 아니거나 만료된 토큰");
+    }
     const readerId = readerData.id;
     const { postId } = req.body;
     const postData = await post.findOne({ where: { id: postId } });
