1c3b6a279534b7356b06255c95f7eafb381d18d4
hi33741 <hi33741@naver.com>
2021-11-08 11:59:14 +0900

    client : { date:11/8 , 작성한 기능 : 프로필이미지업로드 }

--- client/src/Pages/Mypage.js
+++ client/src/Pages/Mypage.js
@@ -1,13 +1,35 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-
+import AWS from 'aws-sdk';
 import axios from 'axios';
-
+import { CgProfile } from 'react-icons/cg';
 const Mypage = ({ setAuthRegi, setIsLogin, authorization, authRegi }) => {
+  console.log('auth', authRegi.profileImg);
+  let token = localStorage.getItem('token');
+
+  const s3 = new AWS.S3({
+    region: 'ap-northeast-2',
+    credentials: new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: process.env.REACT_APP_IdentityPoolId,
+    }),
+  });
+
+  AWS.config.update({
+    region: 'ap-northeast-2',
+    credentials: new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: process.env.REACT_APP_IdentityPoolId,
+    }),
+  });
+
+  const basicImg =
+    'https://gearlogimagestorage.s3.ap-northeast-2.amazonaws.com/%EC%A7%80%EC%9A%B0%EB%A9%B4%EC%95%88%EB%90%98%EB%8A%94+%EA%B8%B0%EB%B3%B8%ED%94%84%EB%A1%9C%ED%95%84%EC%9D%B4%EB%AF%B8%EC%A7%80%EC%9E%85%EB%8B%88%EB%8B%A4.jpg';
   const history = useHistory();
   const [changeName, setChangeName] = useState('');
   const [istrue, setIsTrue] = useState(true);
-
+  const [imgBase64, setImgBase64] = useState(''); // 이미지태그에 미리보기를 하기위한 base64이미지
+  const [originalImg, setOriginalImg] = useState(basicImg); // 여기 오리지날을 서버에 전달
+  // username을 props로 가져오거나 해야합니다..!
+  console.log('123123', authRegi.profileImg);
   const onUsernameChange = (event) => {
     setChangeName(event.target.value);
   };
@@ -108,13 +130,109 @@ const Mypage = ({ setAuthRegi, setIsLogin, authorization, authRegi }) => {
       });
   };
 
+  const handleFileInput = (event) => {
+    const file = event.target.files[0];
+
+    let reader = new FileReader();
+    // onloaded는 read가 완료되면 자동으로 실행되는 콜백
+    // base64형태의 결과물을 상태(base64)에 저장
+    reader.onloadend = async (e) => {
+      const base64 = await reader.result;
+      if (base64) {
+        setImgBase64(base64.toString());
+      }
+      console.log(imgBase64);
+    };
+    // 이벤트에 입력받은 파일이 있을경우 read실행 & 원본이미지 상태저장
+    if (event.target.files[0]) {
+      reader.readAsDataURL(event.target.files[0]);
+      setOriginalImg(event.target.files[0]);
+    }
+  };
+
+  // 저장 버튼 핸들러 -> 서버에 이 이미지를 저장하고 해당유저의 프로필이미지를 이 이미지로 저장하기 위한 핸들러
+  const uploadButtonHandler = (event) => {
+    const file = event.target.files[0];
+
+    s3.deleteObject(
+      {
+        Bucket: 'gearlogimagestorage',
+        Key: 'profileImg_' + authRegi.username + '.jpg',
+      },
+      (err, data) => {
+        if (err) {
+          throw err;
+        }
+        console.log('s3 deleteObject ', data);
+      },
+    );
+
+    const upload = new AWS.S3.ManagedUpload({
+      params: {
+        Bucket: 'gearlogimagestorage',
+        Key: 'profileImg_' + authRegi.username + '.jpg',
+        Body: file,
+      },
+    });
+
+    const promise = upload.promise(); //프로미스객체아닌걸 프로비스의 형태로 만들어준다
+    promise.then(
+      function (data) {
+        axios
+          .patch(
+            'http://52.79.233.29:8080/user/profileimg',
+            {
+              profileImg: data.Location,
+            },
+            {
+              headers: { authorization: `Bearer ${token}` },
+            },
+          )
+          .then((res) => {
+            // authorization();
+          });
+      },
+      function (err) {
+        console.log('s3 이미지 업로드 실패');
+      },
+    );
+  };
+  console.log(authRegi.profileImg);
+  const inputRef = useRef();
   return (
     <div className="my">
       <div className="mypage">
         <h1>My Account</h1>
+        <img className="profile-img" src={authRegi.profileImg}></img>
+
+        <br />
+        <br />
+        <br />
+
+        <label className="custom-file-upload">
+          <button
+            className="pofile-bt"
+            onClick={() => {
+              console.log(inputRef);
+              inputRef.current.click();
+            }}
+          >
+            <CgProfile size="30" color="green" />
+          </button>
+
+          <input
+            className="pofile-upload"
+            ref={inputRef}
+            onChange={uploadButtonHandler}
+            type="file"
+            hidden
+          />
+        </label>
+        <hr></hr>
         <div className="acc">
           {istrue ? (
             <>
+              <br></br>
               <div className="mypage-acc">
                 {authRegi.username} 님 환영합니다
               </div>
