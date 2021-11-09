82109428cfd9c96e87d547fe9cf66a0ba110a380
hi33741 <hi33741@naver.com>
2021-11-09 10:22:53 +0900

    client : { date:11/4 , 작성한 기능 :pr받기전 커밋 }

--- client/src/Pages/UsedView.js
+++ client/src/Pages/UsedView.js
@@ -7,15 +7,13 @@ const UsedView = () => {
         <div className="used-read">
           <div className="used-title">asdf</div>
           <div className="used-info">
-            <span className="used-info-list">asdf</span>
+            <span className="used-info-list">QooQua</span>
 
-            <span className="used-info-list">asdf </span>
+            <span className="used-info-list">6일전</span>
 
-            <span className="used-info-list">asdf</span>
+            <span className="used-info-list">조회</span>
 
-            <span className="used-info-list">sadf </span>
-
-            <span className="used-info-list">asdf </span>
+            <span className="used-info-list">관심 </span>
           </div>
 
           <div
