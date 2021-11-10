596026d92029dac93ddbab2aec2f2c049c99f0c8
Kim dongchan <dongchan1583@gmail.com>
2021-11-10 19:23:31 +0900

    client : {date: 11/10, 작성한 기능: Review page, chat room css 수정}

--- client/src/Pages/Brands/Review/ReviewTemp.js
+++ client/src/Pages/Brands/Review/ReviewTemp.js
@@ -12,6 +12,7 @@ import roccataimo from '../../../Img/brands/Roccat/roccataimo.png';
 import konepro from '../../../Img/brands/Roccat/konepro.png';
 import k70kbd from '../../../Img/brands/Corsair/k70keyboard.png';
 import k70tkl from '../../../Img/brands/Corsair/k70tkl1.png';
+import l7502pd from '../../../Img/brands/Roccat/750pd2.png'
 
 const gearimg = [
   {
@@ -57,6 +58,10 @@ const gearimg = [
   {
     id: 10,
     src: k70tkl,
+  },
+  {
+    id: 11,
+    src: l7502pd,
   },
 ];
 
