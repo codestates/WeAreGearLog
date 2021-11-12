2ebc461d3a72f75420e1ccbd9c114f249257765d
Hyun H <raidi9069@gmail.com>
2021-11-12 09:37:12 +0900

    server : { date:11/12 , 작성한 기능 :소켓아이오 설정코드 분리후 서버 점검3 }

--- server/app.js
+++ server/app.js
@@ -2,19 +2,32 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const router = require("./router");
+const port = 8080;
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const server = require("http").createServer(app);
-const port = process.env.SERVER_PORT || 8080;
+const server = app.listen(port, () => {
+  console.log(port, "번 포트에서 서버가 실행중 입니다.");
+});
 const io = require("socket.io")(server, {
   cors: {
-    origin: "*",
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://gearlog.ga",
+      "https://www.gearlog.ga",
+    ],
+    credentials: true,
   },
 });
 
 app.use(
   cors({
-    origin: true,
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://gearlog.ga",
+      "https://www.gearlog.ga",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PATCH", "OPTIONS", "DELETE"],
   })
@@ -22,9 +35,5 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 app.use(router);
-
-server.listen(port, () => {
-  console.log(port, "번 포트에서 서버가 실행중 입니다.");
-});
 
 require("./socket")(io);
