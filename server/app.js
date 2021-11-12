6af8ec00834da861e1cc28bc4937c1f0da8c4ef8
Hyun H <raidi9069@gmail.com>
2021-11-12 09:08:30 +0900

    server : { date:11/12 , 작성한 기능 :소켓아이오 설정코드 분리후 서버 점검 }

--- server/app.js
+++ server/app.js
@@ -5,15 +5,27 @@ const router = require("./router");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const server = require("http").createServer(app);
+const HTTP_PORT = 8080;
 const io = require("socket.io")(server, {
   cors: {
-    origin: "*",
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://gearlog.ga",
+      "https://www.gearlog.ga",
+    ],
+    credentials: true,
   },
 });
 
 app.use(
   cors({
-    origin: true,
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://gearlog.ga",
+      "https://www.gearlog.ga",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PATCH", "OPTIONS", "DELETE"],
   })
@@ -22,29 +34,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(router);
 
-const HTTP_PORT = 8080;
-
-const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
-
-io.on("connection", (socket) => {
-  console.log(`Client ${socket.id} connected`);
-
-  // 방들어오기
-  const { roomId } = socket.handshake.query;
-  socket.join(roomId);
-
-  // 수신
-  socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
-    io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
-  });
-
-  // 방나가기
-  socket.on("disconnect", () => {
-    console.log(`Client ${socket.id} diconnected`);
-    socket.leave(roomId);
-  });
-});
-
 server.listen(HTTP_PORT, () => {
   console.log(HTTP_PORT, "번 포트에서 서버가 실행중 입니다.");
 });
@@ -52,3 +41,5 @@ server.listen(HTTP_PORT, () => {
 // app.listen(HTTP_PORT, () => {
 //   console.log(HTTP_PORT, "번 포트에서 서버가 실행중 입니다.");
 // });
+
+require("./socket")(io);
