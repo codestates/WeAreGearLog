3eb3bceb8529b3520e572c865f1522c912b6cd43
Hyun H <raidi9069@gmail.com>
2021-11-12 14:38:18 +0900

    server&client : { date:11/12 , 작성한 기능 :소켓아이오 재시도}

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const port = 8080;
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 // const io = require("socket.io")(server, {
 //   cors: {
 //     origin: [
@@ -35,26 +36,26 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(router);
 
-// const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
+const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
 
-// io.on("connection", (socket) => {
-//   console.log(`Client ${socket.id} connected`);
+io.on("connection", (socket) => {
+  console.log(`Client ${socket.id} connected`);
 
-//   // 방들어오기
-//   const { roomId } = socket.handshake.query;
-//   socket.join(roomId);
+  // 방들어오기
+  const { roomId } = socket.handshake.query;
+  socket.join(roomId);
 
-//   // 수신
-//   socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
-//     io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
-//   });
+  // 수신
+  socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
+    io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
+  });
 
-//   // 방나가기
-//   socket.on("disconnect", () => {
-//     console.log(`Client ${socket.id} diconnected`);
-//     socket.leave(roomId);
-//   });
-// });
+  // 방나가기
+  socket.on("disconnect", () => {
+    console.log(`Client ${socket.id} diconnected`);
+    socket.leave(roomId);
+  });
+});
 
 server.listen(port, () => {
   console.log(port, "번 포트에서 서버가 실행중 입니다.");
