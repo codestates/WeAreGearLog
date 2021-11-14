47ec30ac7150f7a4f646701b6f33652358e3a872
hi33741 <hi33741@naver.com>
2021-11-14 18:22:28 +0900

    client : { date:11/14 , 작성한 기능 :채팅 업그레이드 }

--- client/src/Pages/chat/ChatRoom.js
+++ client/src/Pages/chat/ChatRoom.js
@@ -6,31 +6,26 @@ import { RiSendPlaneFill } from 'react-icons/ri';
 import { GrClose } from 'react-icons/gr';
 import { useHistory } from 'react-router-dom';
 const ChatRoom = (props) => {
-  console.log(props);
   const history = useHistory();
   const { roomId } = props.match.params; // Gets roomId from URL
-  const { messages, sendMessage, saveMessage, loadMessage } = useChat(roomId); // Creates a websocket and manages messaging
+  const { messages, sendMessage } = useChat(roomId); // Creates a websocket and manages messaging
   const [newMessage, setNewMessage] = useState(''); // Message to be sent
-
-  useEffect(() => {
-    loadMessage();
-  }, []);
-  useEffect(() => {
-    if (messages.length !== 0) {
-      saveMessage(messages[messages.length - 1]);
-    }
-  }, [messages]);
 
   const handleNewMessageChange = (event) => {
     setNewMessage(event.target.value);
   };
 
-  const handleSendMessage = () => {
-    // console.log(messages);
+  const handleSendMessage = (e) => {
     sendMessage(newMessage);
     setNewMessage('');
   };
 
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      sendMessage(newMessage);
+      setNewMessage('');
+    }
+  };
   return (
     <div className="chat-room-container">
       <div onClick={() => props.setChatOpen(false)} className="chat-close">
@@ -68,6 +63,7 @@ const ChatRoom = (props) => {
       </div>
       <div className="inputbar">
         <textarea
+          onKeyPress={handleKeyPress}
           value={newMessage}
           onChange={handleNewMessageChange}
           placeholder="메세지를 입력해주세요"
